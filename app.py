023a9f5d9b6c77af827a44a170bb281fa4584aef
giorgi <giorgi.beridze@introlab-systems.com>
2018-02-07 18:36:20 +0400

    Working version 1.0.0

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def index():
 @socketio.on("my event")
 def handler(data):
     print(data)
+    socketio.emit("my response", data)
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
