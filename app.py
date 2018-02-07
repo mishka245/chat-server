7176f46961650037c2fd8c952d012c862fb9db11
giorgi <giorgi.beridze@introlab-systems.com>
2018-02-07 18:03:13 +0400

    Front is done.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template("/index/index.html")
+
+@socketio.on("my event")
+def handler(data):
+    print(data)
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+
